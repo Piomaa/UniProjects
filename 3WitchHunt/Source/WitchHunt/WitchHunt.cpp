a10bd7884eb9e08179afa2b57802211948305ced
Piomaa <117256620+Piomaa@users.noreply.github.com>
2026-04-23 12:16:06 +0100

    remove all

--- 3WitchHunt/Source/WitchHunt/WitchHunt.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WitchHunt.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WitchHunt, "WitchHunt" );
